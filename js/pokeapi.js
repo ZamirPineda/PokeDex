34380d9333cf68eaaa96432622ae73adb99abf6c
Zamir Alejandro Pineda Gallego <zamirpg@hotmail.com>
2022-06-09 11:08:48 -0500

    Version 1.0.0
    Arreglos para el localStorage

--- js/pokeapi.js
+++ js/pokeapi.js
@@ -3,13 +3,22 @@ const baseUrl2 = `https://pokeapi.co/api/v2/pokemon-species/`;
 const pokemon = document.getElementById('pokemonName');
 const buttonPokemon = document.getElementById('searchPokemon');
 const buttonClear = document.getElementById('clearPokemon');
+const buttonHistory = document.getElementById('boton-cargar');
 const appNode = document.getElementById('app');
 
 buttonPokemon.addEventListener('click' , insertPokemon);
 buttonPokemon.addEventListener('touchstart' , insertPokemon); //*For mobile devices
 
+document.getElementById('pokemonName').addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    document.getElementById("searchPokemon").click();
+  }
+});
+
 buttonClear.addEventListener('click' , deletePokemons);
 buttonClear.addEventListener('touchstart' , deletePokemons); //* For mobile devices
+
+buttonHistory.addEventListener('click' , cargar);
 
 async function insertPokemon() {
   try {
@@ -28,51 +37,56 @@ async function insertPokemon() {
       result.push([pokemonInfo2 , pokemonDataJSON2[pokemonInfo2]]);
     }
     
-    console.table(result); //! only for development
+    // console.table(result); //! Solo para mi
 
     //* Información de en frente
 
     //*Crear imagen
     const pokemonImage = document.createElement('img');
-    pokemonImage.src = result[14][1].front_default; //*Image of pokemon
+    pokemonImage.src = result[14][1].front_default; //*Imagen del pokemon
 
     //*Nombre de pokemon e ID
     const pokemonName = document.createElement('h2');
-    pokemonName.innerText = `Name: ${result[10][1]} - ID: ${result[6][1]}`; //* Name of pokemon with ID
+    pokemonName.innerText = `Nombre: ${result[10][1]} - ID: ${result[6][1]}`; //* Nombre o ID
 
     //*Tipo de pokemon
     const pokemonType = document.createElement('h2');
-    pokemonType.innerText = `Type: ${result[16][1][0].type.name}`; //*Type of pokemon
+    pokemonType.innerText = `Tipo: ${result[16][1][0].type.name}`; //*Tipo de pokemon
 
     //* Pokemon Altura
     const height = document.createElement('p');
-    height.innerText = `Height: ${result[4][1]}`; 
+    height.innerText = `Altura: ${result[4][1]}`; 
     height.classList.add('pokemonStats');
 
     //* Pokemon Peso
     const weight = document.createElement('p');
-    weight.innerText = `Weight: ${result[17][1]}`; 
+    weight.innerText = `Peso: ${result[17][1]}`; 
     weight.classList.add('pokemonStats');
 
     //* Pokemon Description
     const flavor_text = document.createElement('p');
-    flavor_text.innerText = `Description: ${result[24][1][26].flavor_text}`;
+    flavor_text.innerText = `Descripción: ${result[24][1][26].flavor_text}`;
     flavor_text.classList.add('pokemonStats');
 
     //* Categoria
     const genera = document.createElement('p');
-    genera.innerText = `Categoria: ${result[28][1][5].genus}`; //* Pokemon special attack
+    genera.innerText = `Categoria: ${result[28][1][5].genus}`;
     genera.classList.add('pokemonStats');
 
     //* Contenerdor de stats
     const stats = document.createElement('div');
-    stats.append(height, weight, flavor_text, genera);
+    stats.append(flavor_text, genera, height, weight);
     stats.classList.add('pokemonStatsContainer');
 
     //*Crear contenedor
     const container = document.createElement('div');
     container.append(pokemonImage , pokemonName ,pokemonType, stats);
     container.classList.add('container');
+
+    let nom = document.getElementById("pokemonName").value; 
+    localStorage.setItem('historyName', JSON.stringify(nom));
+    let nombre = localStorage.getItem('historyName');
+    console.log(nombre); //! Solo para mi
 
     allItems.push(container);
 
@@ -90,6 +104,12 @@ function deletePokemons() {
   allPokemon.forEach(pokemon => {
     pokemon.remove(pokemon);
   });
+  document.getElementById("pokemonName").value = "";
 }
 
-  
+function cargar(){                        
+      /*Obtener datos almacenados*/
+      let nombre = localStorage.getItem('historyName');
+      /*Mostrar datos almacenados*/      
+      document.getElementById("nombre").innerText = nombre;
+};   
